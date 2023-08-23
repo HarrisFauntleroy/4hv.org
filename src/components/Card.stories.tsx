1f49d4209a120661cf0110bcadc628a2a05263cc
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-08-24 08:53:42 +1000

    🎨 style(Card.tsx): redesign Card component to match 4HV forum style 🔧 refactor(Card.stories.tsx): update Card story to reflect new design and content 📚 docs(.storybook/main.ts): add staticDirs to Storybook config for serving static files 📦 feat(public/design): add new 4HV logo image for use in Storybook and app

--- src/components/Card.stories.tsx
+++ src/components/Card.stories.tsx
@@ -1,11 +1,15 @@
-import { Card } from "@mantine/core";
 import type { Meta, StoryObj } from "@storybook/react";
+import { Card } from "./Card";
 
 const meta: Meta<typeof Card> = {
   title: "components/Card",
   component: Card,
   parameters: {
-    assets: ["./assets/4hv.org_.png"],
+    design: {
+      type: "link",
+      label: "4hv",
+      url: "https://4hv.org/",
+    },
   },
 };
 
@@ -14,9 +18,19 @@ export default meta;
 type Story = StoryObj<typeof Card>;
 
 export const Primary: Story = {
-  args: {},
-  parameters: {},
   render: () => {
-    return <Card>Jello</Card>;
+    return (
+      <Card>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ipsum
+        arcu. Donec mattis est in nisl efficitur, molestie congue quam eleifend.
+        Sed dignissim urna urna, eleifend aliquet augue sodales eget.
+        Pellentesque quis tincidunt elit. Quisque ut nibh nisi. Morbi pharetra,
+        justo at hendrerit fringilla, enim risus scelerisque arcu, sit amet
+        pharetra metus est vitae diam. Integer erat velit, facilisis finibus
+        euismod eget, consequat vitae elit. Pellentesque malesuada ligula lacus,
+        sed sollicitudin eros fermentum eu. Pellentesque porta pulvinar luctus.
+        Suspendisse nec leo ligula. Suspendisse eget dolor diam.
+      </Card>
+    );
   },
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,70 @@
-import { Card as MantineCard } from "@mantine/core";
-import { PropsWithChildren } from "react";
+import { Box, Flex, Image, Card as MantineCard, Text } from "@mantine/core";
+import { CSSProperties, PropsWithChildren } from "react";
+
+const cardHeaderStyle: CSSProperties = {
+  height: "18px",
+  fontSize: "10px",
+  fontFamily: "verdana, tahoma, arial, sans-serif",
+  color: "#FFF",
+  background: "linear-gradient(#c7d3e6 10%, #869ac0 40%, #8d9fc2 50%)",
+  borderBottom: "1px solid black",
+};
+
+const cardContentStyle: CSSProperties = {
+  padding: "4px 5px 5px 4px",
+  // boxShadow: "0 0 8px #000",
+  flex: "1",
+};
+
+interface GridHeaderProps extends PropsWithChildren {
+  title: string;
+}
+
+const GridHeader = ({ title, children }: GridHeaderProps) => (
+  <Flex style={cardHeaderStyle}>
+    <Flex
+      bg="linear-gradient(#6c7e9e 50%, #5b6c8e 50%)"
+      gap="4px"
+      align="center"
+      h="100%"
+      pr="24px"
+      style={{ borderRadius: "0 11px 3px 0" }}
+    >
+      <Image
+        alt="Blue circle"
+        width="16"
+        height="16"
+        src="/images/bullet2.gif"
+      />
+      <Text>{title}</Text>
+    </Flex>
+    {children}
+  </Flex>
+);
+
+const GridContent = ({ children }: PropsWithChildren) => (
+  <Box style={cardContentStyle}>{children}</Box>
+);
 
 export function Card<T>({ children }: PropsWithChildren<T>) {
   return (
-    <MantineCard shadow="lg" withBorder style={{ background: "red" }}>
-      {children}
+    <MantineCard
+      withBorder
+      style={{
+        fontFamily: "verdana, tahoma, arial, helvetica, sans-serif",
+        fontSize: "10px",
+        background: "#DAE7F3",
+        color: "#000",
+        borderRadius: "4px",
+        boxShadow:
+          "#505559 0px 0px 1px 2px, #5a5e62 0px 4px 6px -1px, #8b8d90 0px 1px 0px inset",
+      }}
+      p={0}
+    >
+      <GridHeader title="Members Birthdays:" />
+      <GridContent> {children}</GridContent>
     </MantineCard>
   );
 }
+
+// 4HV element background #DAE7F3
