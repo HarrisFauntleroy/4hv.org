314eb15ecf6eae6839395ebfb20f65756c094019
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-08-24 21:40:35 +1000

    📦 feat(package.json): add @mantine/next package to support server-side rendering of Mantine components 🔧 refactor(_document.tsx): replace Chakra UI's ColorModeScript with Mantine's ServerStyles for better server-side rendering support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,42 +1,58 @@
-/**
- *
- * Custom _document
- *
- */
-import { ColorModeScript } from "@chakra-ui/react";
-import { Head, Html, Main, NextScript } from "next/document";
-import React from "react";
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 
-const Document = () => {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <body>
-        <ColorModeScript initialColorMode="system" />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+const stylesServer = createStylesServer();
 
-export default Document;
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
